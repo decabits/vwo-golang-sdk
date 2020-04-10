c32ea13484f62621c7ca0c77e73ef33e0355d048
vaibhavsethia <vaibhavsethia1998@gmail.com>
2020-04-10 19:56:48 +0530

    segmentor test

---
+++ core/segmentor_test.go
@@ -0,0 +1,110 @@
+package core
+
+import (
+	"fmt"
+	"encoding/json"
+	"testing"
+
+	"github.com/decabits/vwo-golang-sdk/schema"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEvaluate(t *testing.T) {
+	options := schema.Options{}
+
+	operator := "and"
+	res := []bool{true, true, true, true}
+	actual := evaluate(operator, res, options)
+	assert.True(t, actual, "Value expected to be true")
+
+	res = []bool{true, false, true, true}
+	actual = evaluate(operator, res, options)
+	assert.False(t, actual, "Value expected to be true")
+
+	operator = "or"
+	res = []bool{false, false, false, false}
+	actual = evaluate(operator, res, options)
+	assert.False(t, actual, "Value expected to be true")
+
+	res = []bool{false, false, false, true}
+	actual = evaluate(operator, res, options)
+	assert.True(t, actual, "Value expected to be true")
+}
+
+func TestOperandUserParser(t *testing.T) {
+	options := schema.Options{
+		VWOUserID: "Robert",
+	}
+	operand := "Robert, Lizzie, Chris, Gimmy, Matt"
+	value := operandUserParser(operand, options)
+	assert.True(t, value, "User expected to be present")
+
+	options = schema.Options{
+		VWOUserID: "Robert",
+	}
+	operand = "Lizzie, Chris, Gimmy, Matt"
+	value = operandUserParser(operand, options)
+	assert.False(t, value, "User expected to be absent")
+}
+
+func TestExtractResult(t *testing.T) {
+	operandType := 1
+	operandValue := "tempval"
+	tagValue := "TempVal"
+	value := extractResult(operandType, operandValue, tagValue)
+	assert.True(t, value, "Incorrect check")
+
+	operandValue = "tempval"
+	tagValue = "TempVool"
+	value = extractResult(operandType, operandValue, tagValue)
+	assert.False(t, value, "Incorrect check")
+
+	operandType = 2
+	operandValue = "tempval"
+	tagValue = "pval"
+	value = extractResult(operandType, operandValue, tagValue)
+	assert.True(t, value, "Incorrect check")
+
+	operandValue = "tempval"
+	tagValue = "pVal"
+	value = extractResult(operandType, operandValue, tagValue)
+	assert.False(t, value, "Incorrect check")
+
+	operandType = 3
+	operandValue = "tempval"
+	tagValue = "val"
+	value = extractResult(operandType, operandValue, tagValue)
+	assert.True(t, value, "Incorrect check")
+
+	operandValue = "tempval"
+	tagValue = "Val"
+	value = extractResult(operandType, operandValue, tagValue)
+	assert.False(t, value, "Incorrect check")
+
+	operandType = 4
+	operandValue = "tempval"
+	tagValue = "tem"
+	value = extractResult(operandType, operandValue, tagValue)
+	assert.True(t, value, "Incorrect check")
+
+	operandValue = "tempval"
+	tagValue = "temv"
+	value = extractResult(operandType, operandValue, tagValue)
+	assert.False(t, value, "Incorrect check")
+
+	operandType = 5
+	operandValue = "tempval"
+	tagValue = "mp"
+	value = extractResult(operandType, operandValue, tagValue)
+	assert.True(t, value, "Incorrect check")
+
+	operandValue = "tempval"
+	tagValue = "MP"
+	value = extractResult(operandType, operandValue, tagValue)
+	assert.False(t, value, "Incorrect check")
+}
+
+func TestEvaluateCustomVariables(t *testing.T) {
+	
+
+}
---
+++ utils/segments_test.go
@@ -0,0 +1,24 @@
+package utils
+
+import (
+	"github.com/decabits/vwo-golang-sdk/constants"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestProcessCustomVariablesValue(t *testing.T) {
+	var value interface{}
+	value = true 
+	actual := ProcessCustomVariablesValue(value)
+	expected := "true"
+	assert.Equal(t, expected, actual, "String mismatch")
+
+	value = "Value" 
+	actual = ProcessCustomVariablesValue(value)
+	expected = "Value"
+	assert.Equal(t, expected, actual, "String mismatch")
+}
+
+func TestMatchWithRegex(t *testing.T) {
+	
+}
