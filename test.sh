1b39d65a0ca5330e4650cf290d7405f0ba096bfa
Piyushhbhutoria <piyush.bhutoria98@gmail.com>
2020-04-26 00:02:29 +0530

    testing travis

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ pwd
 set -e
 echo "" > coverage.txt
 for d in $(go list ./... | grep -v vendor); do
-    pwd
+    echo $d
     go test -race -coverprofile=profile.out -covermode=atomic $d
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
