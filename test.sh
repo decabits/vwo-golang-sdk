909630561f43ead5bc0a1a66935bef7c87bae3d0
Piyushhbhutoria <piyush.bhutoria98@gmail.com>
2020-04-26 17:58:20 +0530

    fixed

--- test.sh
+++ test.sh
@@ -4,10 +4,10 @@ set -e
 echo "" > coverage.txt
 for d in $(go list ./... | grep -v vendor); do
     echo $d
-    go test -coverprofile=profile.out $d
+    go test -race -coverprofile=profile.out -covermode=atomic $d
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
-        # go tool cover -html=profile.out
+        go tool cover -html=profile.out
         rm profile.out
     fi
 done
