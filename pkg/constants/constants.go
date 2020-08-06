674d70da32f9b7bfca0372c4a5cb57186fe8111d
vaibhavsethia <vaibhavsethia1998@gmail.com>
2020-08-06 16:57:21 +0530

    Changed SDK Version to 1.8.0

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -21,7 +21,7 @@ const (
 	MaxTrafficPercent = 100
 	MaxTrafficValue   = 10000
 	StatusRunning     = "RUNNING"
-	SDKVersion        = "1.1.0"
+	SDKVersion        = "1.8.0"
 	SDKName           = "vwo-go-sdk"
 	Platform          = "server"
 	SeedValue         = 1
